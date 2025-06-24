b443b13d7ed4d3ba9add2f2926e68090754d32a0
Thinka Boutiter <thinkaboutiter@gmail.com>
2025-06-24 23:47:20 +0300

    add entity types

---
+++ foobar/app/src/main/java/com/cool/element/foobar/domain/entity/CarNetwork.kt
@@ -0,0 +1,10 @@
+package com.cool.element.foobar.domain.entity
+
+import com.google.gson.annotations.SerializedName
+
+data class CarNetwork(
+    val title: String,
+    @SerializedName("href") val aboutUrlString: String,
+    @SerializedName("description") val details: String,
+    @SerializedName("thumbnail") val imageUrlString: String,
+)
---
+++ foobar/app/src/main/java/com/cool/element/foobar/domain/entity/CarNetworkResponse.kt
@@ -0,0 +1,8 @@
+package com.cool.element.foobar.domain.entity
+import com.google.gson.annotations.SerializedName
+
+data class CarNetworkResponse(
+    val title: String,
+    val version: String,
+    @SerializedName("results") val cars: List<CarNetwork>
+)
