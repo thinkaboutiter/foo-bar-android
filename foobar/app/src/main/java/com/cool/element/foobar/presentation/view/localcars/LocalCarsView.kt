0ccc9c2331741348d24489961b48b25ce3ffa989
Thinka Boutiter <thinkaboutiter@gmail.com>
2025-06-30 23:48:43 +0300

    add base tab coposables

---
+++ foobar/app/src/main/java/com/cool/element/foobar/presentation/view/localcars/LocalCarsView.kt
@@ -0,0 +1,11 @@
+package com.cool.element.foobar.presentation.view.localcars
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LocalCarsView(
+    modifier: Modifier
+) {
+
+}
---
+++ foobar/app/src/main/java/com/cool/element/foobar/presentation/view/networkcars/NetworkCarsView.kt
@@ -0,0 +1,11 @@
+package com.cool.element.foobar.presentation.view.networkcars
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NetworkCarsView(
+    modifier: Modifier
+) {
+
+}
