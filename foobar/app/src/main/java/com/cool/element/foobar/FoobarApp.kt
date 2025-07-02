5a0a154eac86d1e8c67c229178dadcf985a8860c
Thinka Boutiter <thinkaboutiter@gmail.com>
2025-07-03 02:44:25 +0300

    add test db records

--- foobar/app/src/main/java/com/cool/element/foobar/FoobarApp.kt
+++ foobar/app/src/main/java/com/cool/element/foobar/FoobarApp.kt
@@ -3,8 +3,10 @@ package com.cool.element.foobar
 import android.app.Application
 import androidx.room.Room
 import com.cool.element.foobar.data.datasource.local.AppRoomDatabaseA
+import com.cool.element.foobar.domain.entity.local.CarLocal
+import kotlinx.coroutines.runBlocking
 
-class FoobarApp: Application() {
+class FoobarApp : Application() {
 
     val roomDatabase by lazy {
         Room.databaseBuilder(
@@ -12,5 +14,33 @@ class FoobarApp: Application() {
             AppRoomDatabaseA::class.java,
             AppRoomDatabaseA.DATABASE_NAME
         ).build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        runBlocking {
+            createDatabaseRecords()
+        }
+    }
+
+    private suspend fun createDatabaseRecords() {
+        val dao = roomDatabase.carDao()
+        for (i in 1..10) {
+            // check for car existence
+            val existingCar = dao.getCarById(i.toLong())
+            if (existingCar != null) {
+                continue // Skip if the car already exists
+            }
+
+            val car = CarLocal(
+                i.toLong(),
+                "DB Car $i",
+                "http",
+                "${2020 + i}",
+                "${4000 + 1}"
+            )
+            dao.insertCar(car)
+        }
     }
 }
