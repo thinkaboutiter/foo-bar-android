cb0d34ddee503b51987fc965f9ec5dfd027d9b2e
Thinka Boutiter <thinkaboutiter@gmail.com>
2025-07-04 21:30:17 +0300

    configure schema directory of room database

--- foobar/app/build.gradle.kts
+++ foobar/app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.androidx.room)
     jacoco
 }
 
@@ -42,6 +43,10 @@ android {
     }
     buildFeatures {
         compose = true
+    }
+    
+    room {
+        schemaDirectory("$projectDir/schemas")
     }
     
     testOptions {
